a69bae3763a700159118fcd0670202014d3bbc3d
xiaopin <pincheng23@hotmail.com>
2018-05-14 17:50:26 +0800

    增加文档说明

--- Example/InteractivePushGesture/UIViewController+InteractivePushGesture.h
+++ Example/InteractivePushGesture/UIViewController+InteractivePushGesture.h
@@ -11,10 +11,12 @@
 @protocol UIViewControllerInteractivePushGestureDelegate;
 
 
+/// 给控制器增加一个左滑push的手势功能
 @interface UIViewController (InteractivePushGesture)
 
-/// default `NO`
+/// 是否启用左滑push手势, 默认 `NO`
 @property (nonatomic, assign, getter=isInteractivePushGestureEnabled) IBInspectable BOOL interactivePushGestureEnabled;
+/// 代理
 @property (nonatomic, weak) id<UIViewControllerInteractivePushGestureDelegate> interactivePushGestureDelegate;
 
 @end
@@ -23,6 +25,12 @@
 @protocol UIViewControllerInteractivePushGestureDelegate <NSObject>
 
 @required
+/**
+ 返回需要被push的新控制器
+
+ @param fromViewController 当前控制器(执行push操作的控制器)
+ @return 被push的控制器
+ */
 - (UIViewController *)destinationViewControllerFromViewController:(UIViewController *)fromViewController;
 
 @end
